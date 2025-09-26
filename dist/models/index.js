5e397ddb72c2cdc61b6047bcefef05266ecdd4cb
diegopolancolozano <118563766+diegopolancolozano@users.noreply.github.com>
2025-09-25 23:31:48 -0500

    dockerfile and more structure

---
+++ dist/models/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Student = void 0;
+var student_model_1 = require("./student.model");
+Object.defineProperty(exports, "Student", { enumerable: true, get: function () { return __importDefault(student_model_1).default; } });
+// export { default as University, IUniversity } from './university.model';
+// export { default as Professor, IProfessor } from './professor.model';
+// export { default as Comment, IComment } from './comment.model';
+//# sourceMappingURL=index.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/connectionDB';
+
 const app = express();
-app.get('/api/health', (_req, res) => res.json({ ok: true }));
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Qualifica o seu Professor - API' });
+});
+
+app.get('/api/health', (_req, res) => {
+  res.json({ ok: true });
+});
+
 export default app;
