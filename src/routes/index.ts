03f1c77ce2f78b0e8090a4cc576257801aad48d1
Sebastian Erazo Ochoa <sebastianerazo266@gmail.com>
2025-09-27 02:33:26 -0500

    chore(routes): wire up feature routers in index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,13 @@
-export {};
+import { Router } from 'express';
+import auth from './auth.routes';
+import users from './users.routes';
+import universities from './universities.routes';
+import professors from './professors.routes';
+
+const r = Router();
+r.use('/auth', auth);
+r.use('/users', users);
+r.use('/universities', universities);
+r.use('/professors', professors);
+
+export default r;
